6d1df8e80f3922513ac5523ba317bdaadce8672f
Svenja Uhlemeyer <64146042+SUhlemeyer@users.noreply.github.com>
2023-03-31 11:21:38 +0200

    Delete background_ignore.py

--- background_ignore.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-from PIL import Image
-
-for im in os.listdir('/home/uhlemeyer/outputs/Cluster_experiment1_run10/human/semantic_id_all'):
-    label = np.array(Image.open('/home/uhlemeyer/outputs/Cluster_experiment1_run10/human/semantic_id_all/' + im))
-    label_ignore = np.copy(label)
-    label_ignore[label < 200] = 0
-    Image.fromarray(label_ignore.astype('uint8')).save('/home/uhlemeyer/outputs/Cluster_experiment1_run10/human/semantic_id/' + im)
